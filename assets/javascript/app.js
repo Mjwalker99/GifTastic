2968a3dd40f852c71d18886f8f42120a4c39e59c
Micah Walker <minimelon19@gmail.com>
2018-09-19 10:40:51 -0500

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,27 @@
-var topics = ["Vince Staples", "A$ap Rocky", "Kendrick Lamar", "Future", "Drake", "Travis Scott"];
+$(document).ready(function() {
 
-$("button").on("click", function() {
-    var topics = $(this).attr("data-person");
 
+var rappers = ["Vince Staples", "A$ap Rocky", "Kendrick Lamar", "Future", "Drake", "Travis Scott"];
+
+for (var i = 0; i < rappers.length; i++) {
+
+    var j = $('<button>') 
+    j.addClass('rappers'); 
+    j.attr('data-name', rappers[i]);
+    j.text(rappers[i]); 
+    $('.button').append(j); 
+
+}
 
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    topics + "&api_key=7efG4IOfEuxLwvMi3tt1SNF6P5bsT7oP&limit=10";
+    rappers + "&api_key=7efG4IOfEuxLwvMi3tt1SNF6P5bsT7oP&limit=10";
 
 
 $.ajax({
     url: queryURL,
     method: "GET" 
-})
 
-.then(function(response) {
+
 
 });
 });
